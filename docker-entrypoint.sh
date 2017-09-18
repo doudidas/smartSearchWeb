e3867d3e2220ba0a145e4b69a352baa18935e786
Edouard Topin <ed.topin@gmail.com>
2017-09-19 00:10:54 +0100

    export ip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@
 # 2nd parameter is port
 if [ $2 = '' ]
 then
-  echo "export ip_adress_engine=$1:$2"
-   grunt serve
-else
   echo "docker run spacelama/api [mongo ip] [mongo port]"
+else
+  export ip_adress_engine=$1:$2
+  grunt serve
 fi
