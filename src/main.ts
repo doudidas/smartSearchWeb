21796e3a8538fd7468072105e544dbb0c27e7fce
Edouard Topin <ed.topin@gmail.com>
2018-05-04 12:39:37 +0200

    auto commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+///<reference path="../node_modules/@angular/platform-browser-dynamic/src/platform-browser-dynamic.d.ts"/>
 import './polyfills.ts';
 
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
