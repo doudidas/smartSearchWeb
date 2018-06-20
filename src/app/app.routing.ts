48ed60d1f73ff1bb4605b3770585f905106ce208
Edouard Topin <ed.topin@gmail.com>
2018-06-20 14:24:33 +0200

    topic by ID added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -21,6 +21,7 @@ export const ROUTES: Routes = [
     { path: 'login', component: LoginComponent },
   //  { path: 'vro', component: VroComponent },
     { path: "snake", component: SnakeComponent },
+    { path: 'topic/:id', component: TopicComponent },
     { path: "topic", component: TopicComponent },
     { path: 'users', component: UsersComponent },
 ];
--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Topic } from '../class/topic';
 import { ApiService } from '../services/api.service';
-
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
@@ -12,12 +13,19 @@ export class TopicComponent implements OnInit {
   public topics: Topic[];
   public focusTopic: Topic;
   public showCard: boolean;
-  constructor(private api: ApiService) {
+
+  constructor(private api: ApiService, private route: ActivatedRoute) {
     this.getAllTopics();
   }
 
-  ngOnInit() {
-    this.focusTopic = new Topic(null, null, null);
+  async ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    if (id != null) {
+      this.focusTopic = this.getTopicById(id);
+      this.showCard = true;
+    } else {
+      this.focusTopic = new Topic(null, null, null);
+    }
     this.showCard = false;
   }
 
@@ -33,6 +41,18 @@ export class TopicComponent implements OnInit {
       );
     }
   }
+
+  public getTopicById(topicId: number) {
+    let output: Topic;
+    for (let i = 0; i < this.topics.length; i++) {
+      if (this.topics[i].hasOwnProperty('id') && this.topics[i].id === topicId) {
+        output = this.topics[i];
+        break;
+      }
+    }
+    return output;
+  }
+
   getTopicUrlById(topicId: number) {
     let uri = "images/topics/full/" + Math.floor(topicId) + ".jpg";
     return uri;
