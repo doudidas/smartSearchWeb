4276633a34dd3587c335fdd8a6e07afaab0476c5
Edouard Topin <ed.topin@gmail.com>
2019-04-01 12:03:23 +0200

    script updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-docker build -t spacelama/web:latest .;
-docker push spacelama/web:latest;
-docker rmi spacelama/web:latest;
+echo Please choose branch to deploy ? [master/dev] default: dev
+read branch
+
+if [ $branch == "master" ]
+then
+    tag="latest"
+else
+    tag="dev"
+fi
+
+docker build -t spacelama/web:$tag .;
+docker push spacelama/web:$tag;
+docker rmi spacelama/web:$tag;
