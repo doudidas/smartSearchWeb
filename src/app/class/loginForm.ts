c83d4c9309c347d796b62b2cd58d52ba52d96cb6
Edouard Topin <ed.topin@gmail.com>
2019-09-04 15:25:21 +0200

    version: 0.6

--- src/app/class/loginForm.ts
+++ src/app/class/loginForm.ts
@@ -2,6 +2,7 @@ export class LoginForm {
     constructor(
         public username: string,
         public password: string,
+        public type: string,
         public rememberMe: string
     ) {
     }
