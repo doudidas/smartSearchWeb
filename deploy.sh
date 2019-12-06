7264f646eb0e2e519ea4dc4d1a8308274240b318
Edouard Topin <ed.topin@gmail.com>
2019-12-06 21:07:53 +0100

    version: 1.17

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,18 @@ then
 else
     tag="dev"
 fi
-echo "Deploying into $tag branch…"
+
+echo "Deploying image:$tag on Docker.io"
 docker build -t spacelama/web:$tag .;
 docker push spacelama/web:$tag;
+echo "Done !"
+echo "Deploying image:$tag on Github"
+
+# Step 0: Get version
+version=$(cat ./version.txt)
+# Step 1: Authenticate
+docker login docker.pkg.github.com --username doudidas
+# Step 2: Tag
+docker tag spacelama/web:$tag docker.pkg.github.com/doudidas/smartsearchweb/spacelama:$version
+# Step 3: Publish
+docker push docker.pkg.github.com/doudidas/smartsearchweb/spacelama:$version
