446cab5a9ac7023b0fbc7d050282520d26de5ce1
Edouard Topin <ed.topin@gmail.com>
2021-02-26 18:02:17 +0100

    Update session.service.ts
    more comments

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -2,14 +2,26 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginSession } from '../class/loginSession';
 
+// Who can call this service
 @Injectable({
   providedIn: 'root'
 })
+
 export class SessionService {
+  
+  // Minimal Constructor with router only
+  constructor(private router: Router,) { }
+  
+  // Static function are use to avoid manipulating a local SessionService Variable.
+  
+  // Function to remove User information from both session and local Cache 
   static logOut() {
     localStorage.removeItem("login");
     sessionStorage.removeItem("login");
   }
+  
+  // Funciton to get User Informations form session cache
+  // return null if no session found !
   static getSession(): LoginSession {
     try {
       return JSON.parse(sessionStorage.getItem("login"))
@@ -17,17 +29,24 @@ export class SessionService {
       return null
     }
   }
-  constructor(private router: Router,) { }
+  
+  /* function to store login info in cache.
+    Local Storage persist with differents tabs and when navigator is terminated
+    Session Storage is different per tabs and is whiped with navigator shutdow
+  */
   public logOn(session: LoginSession) {
+    // we store with Hashed password
     session.generateHash();
-    sessionStorage.setItem('login', JSON.stringify(session))
+    
     if (session.rememberMe) {
       localStorage.setItem('login', JSON.stringify(session))
-    } else {
-      sessionStorage.setItem('login', JSON.stringify(session))
-    }
+    } 
+    
+    sessionStorage.setItem('login', JSON.stringify(session))
+    
     this.router.navigate(['home']);
   }
+  // check if the user is logged
   public isLogged(): boolean {
     return SessionService.getSession() != null
   }
