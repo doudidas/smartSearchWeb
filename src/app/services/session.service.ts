cd5fcdcd9f0b32ddc6b82516d77f1b6c21002ba3
Edouard Topin <ed.topin@gmail.com>
2021-02-26 18:04:10 +0100

    version: 1.33

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -8,18 +8,18 @@ import { LoginSession } from '../class/loginSession';
 })
 
 export class SessionService {
-  
+
   // Minimal Constructor with router only
   constructor(private router: Router,) { }
-  
+
   // Static function are use to avoid manipulating a local SessionService Variable.
-  
+
   // Function to remove User information from both session and local Cache 
   static logOut() {
     localStorage.removeItem("login");
     sessionStorage.removeItem("login");
   }
-  
+
   // Funciton to get User Informations form session cache
   // return null if no session found !
   static getSession(): LoginSession {
@@ -29,7 +29,7 @@ export class SessionService {
       return null
     }
   }
-  
+
   /* function to store login info in cache.
     Local Storage persist with differents tabs and when navigator is terminated
     Session Storage is different per tabs and is whiped with navigator shutdow
@@ -37,13 +37,13 @@ export class SessionService {
   public logOn(session: LoginSession) {
     // we store with Hashed password
     session.generateHash();
-    
+
     if (session.rememberMe) {
       localStorage.setItem('login', JSON.stringify(session))
-    } 
-    
+    }
+
     sessionStorage.setItem('login', JSON.stringify(session))
-    
+
     this.router.navigate(['home']);
   }
   // check if the user is logged
